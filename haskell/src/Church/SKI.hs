fd552fa9f4ecbcf84ece4beac3d2794216ebffca
Jeff <jeffreydwyer95@hotmail.com>
2017-08-16 16:36:58 -0700

    Removed fix comment

--- haskell/src/Church/SKI.hs
+++ haskell/src/Church/SKI.hs
@@ -17,9 +17,6 @@ i = s k k
 (.) :: (b -> c) -> (a -> b) -> (a -> c)
 (.) = s (k s) k
 
---fix = let op = (k (s i i))
---        in s op (s (.) op)
-
 fix f = let x = f x in x
 
 flip :: (a -> b -> c) -> (b -> a -> c)
